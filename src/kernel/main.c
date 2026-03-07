3f7940f80a7f39754163f7f5fe6b747c7414b5b5
RicardoMC310 <ricardo.matos72.etec@gmail.com>
2026-03-07 20:19:39 -0300

    adicionando sistema de trocar card

--- src/kernel/main.c
+++ src/kernel/main.c
@@ -1,18 +1,31 @@
 #include "hd/millis.h"
+#include "hd/delay.h"
+#include "card/card.h"
 #include <avr/io.h>
 #include <avr/interrupt.h>
 
-void setup_kernel() {
-    DDRB |= (1 << PB5);
-    PORTB &= ~(1 << PB5);
+void check_change_button_card();
+
+void setup_kernel()
+{
+    DDRD &= ~(1 << PD2);  // entrada
+    PORTD &= ~(1 << PD2); // pull-up DESLIGADO
+
+    card_init();
+
 }
 
-void loop_kernel() {
-    static uint32_t lastTime = 0;
-    uint32_t currentTime = millis1();
-    
-    if (currentTime - lastTime >= 1000) {
-        lastTime = currentTime;
-        PORTB ^= (1 << PB5);
+void loop_kernel()
+{
+    check_change_button_card();    
+    card_update();
+}
+
+void check_change_button_card()
+{
+    if (PIND & (1 << PD2))
+    {
+        card_change();
+        delay(250);
     }
 }
